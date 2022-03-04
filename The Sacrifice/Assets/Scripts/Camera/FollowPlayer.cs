557326a828af4c02faed2205f4c302ce1d93b4be
Oliver-Rasmussen <73238075+Oliver-Rasmussen@users.noreply.github.com>
2022-03-04 15:24:48 +0100

    Camera Deadzone

--- The Sacrifice/Assets/Scripts/Camera/FollowPlayer.cs
+++ The Sacrifice/Assets/Scripts/Camera/FollowPlayer.cs
@@ -7,19 +7,26 @@ public class FollowPlayer : MonoBehaviour
     [HideInInspector]
     public Rigidbody2D target;
 
-    public float smoothSpeed = 5f;
+    public float deadzone = 1f;
+    public float smoothSpeed = 15;
     public Vector3 offSet;
+
+    private Vector3 damp = Vector3.zero;
 
     private void Awake()
     {
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
-        transform.position = target.position;
+        transform.position = (Vector3)target.position - offSet;
     }
+
 
     private void FixedUpdate()
     {
-        Vector3 desiredPosition = target.transform.position - offSet;
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
-        transform.position = smoothedPosition;
+        if(Vector2.Distance(transform.position, target.position) >= deadzone)
+        {
+            Vector3 desiredPosition = target.transform.position - offSet;
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+            transform.position = Vector3.SmoothDamp(transform.position, smoothedPosition, ref damp, 0.1f);
+        }
     }
 }
