9857bd4a900a0f10aaa4fe74650283f24ffc532b
gosuenesy <82447771+gosuenesy@users.noreply.github.com>
2022-04-04 09:15:00 +0200

    different loot drops + npc skill point merchant

--- The Sacrifice/Assets/Scripts/Enemy/EnemyBehavior.cs
+++ The Sacrifice/Assets/Scripts/Enemy/EnemyBehavior.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class EnemyBehavior : MonoBehaviour
@@ -8,6 +9,7 @@ public class EnemyBehavior : MonoBehaviour
     public float health;
     private float healthMax;
     public HealthbarBehavior healthbar;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -28,6 +30,15 @@ public class EnemyBehavior : MonoBehaviour
         if (health <= 0) {
             animator.SetTrigger("TriggerDeath");
             healthbar.slider.gameObject.SetActive(false);
+            //Loot drop
+            LootManager lm = GameObject.Find("LootContainer").GetComponent<LootManager>();
+            switch(gameObject.tag) {
+                case "EnemyMelee1": lm.GenEnemy1Loot(transform); break;
+                case "EnemyMelee2": lm.GenEnemy2Loot(transform); break;
+                case "EnemyMelee3": lm.GenEnemy3Loot(transform); break;
+                case "NpcMerchant": lm.GenNpcMerchantLoot(transform); break;
+            }
+            Destroy(this.gameObject);
         }
     }
 }
