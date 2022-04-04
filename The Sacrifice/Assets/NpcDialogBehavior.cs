8853963671c01d6c24f0dd9ea9dee67ec3e489f2
Andreas <saerdna.petersen@gmail.com>
2022-04-04 11:26:16 +0200

    sum stuf work sum stuf done

--- The Sacrifice/Assets/NpcDialogBehavior.cs
+++ The Sacrifice/Assets/NpcDialogBehavior.cs
@@ -11,24 +11,31 @@ public class NpcDialogBehavior : MonoBehaviour
     private GameObject panel;
     [SerializeField]
     private Vector3 offset;
+    [SerializeField]
     private Transform player;
+    [SerializeField]
     private StatManager stats;
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player").transform;
+        //player = GameObject.FindGameObjectWithTag("Player").transform;
         panel.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Vector2.Distance(player.position, transform.parent.position) > 2) {
+        if (Vector2.Distance(player.position, transform.parent.position) > 1.5f)
+        {
             panel.SetActive(false);
-        } else {
+        }
+        else
+        {
             panel.SetActive(true);
-            if(Input.GetKeyDown(KeyCode.E)) {
-                if (PlayerStats.Currency >= 3) {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (PlayerStats.Currency >= 3)
+                {
                     PlayerStats.Currency -= 3;
                     stats.currentPoints += 1;
                 }
